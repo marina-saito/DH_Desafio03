9375afd8b2095ade3a0bd325fa754232004df12b
Marina <marina.ssaito@gmail.com>
2020-11-28 21:31:03 -0300

    Created LoginActivity

--- app/src/main/java/com/example/desafio03/ui/LoginActivity.kt
+++ app/src/main/java/com/example/desafio03/ui/LoginActivity.kt
@@ -3,10 +3,12 @@ package com.example.desafio03.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.desafio03.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
