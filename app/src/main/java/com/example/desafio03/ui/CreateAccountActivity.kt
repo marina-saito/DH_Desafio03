2bcdccecb55cb991d6caac8736a66cb43b8d3876
Marina <marina.ssaito@gmail.com>
2020-11-29 21:17:32 -0300

    Creating CreateAccountActivity

---
+++ app/src/main/java/com/example/desafio03/ui/CreateAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.example.desafio03.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.desafio03.R
+
+class CreateAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
